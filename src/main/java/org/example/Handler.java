4141b2285f7af7a9d4a3542f3e589bfbb42e0721
AlKham <greenowlkitty@gmail.com>
2024-09-09 22:21:02 +0500

    Внесены правки в соответствии с задачей Handler

---
+++ src/main/java/org/example/Handler.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.io.BufferedOutputStream;
+
+@FunctionalInterface
+public class Handler {
+    public static void handle(Request request, BufferedOutputStream responseStream) {
+
+    }
+}
